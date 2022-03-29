b26d27f56f3bb9a02b950a869884964ff29efff7
Iúri Raimundo <iuraimundo@deloitte.pt>
2022-03-29 22:27:32 +0100

    added composer file

--- api/auth.php
+++ api/auth.php
@@ -11,18 +11,19 @@
     $users_json_string = file_get_contents($USERS_FILE_PATH);
     $users = json_decode($users_json_string, true);
 
-    // Obter corpo do pedido
+    // Obter corpo do pedido e validar
     $req_body = json_decode(file_get_contents('php://input'), true);
 
     if ($req_body == NULL || !validateAuthBody($req_body)) {
         wrongFormatResponse();
     }
 
+    // Iterar utilizadores existentes no sistema e tentar iniciar a sessão
     foreach ($users as $user) {
         login($user, $req_body["username"], $req_body["password"]);
     }
 
-    // Se chegar aqui é porque não conseguiu iniciar sessão
+    // Se chegar aqui é porque não conseguiu iniciar a sessão
     http_response_code(401);
     echo json_encode(array(
         "message" => "O nome de utilizador ou password estão incorretos."
