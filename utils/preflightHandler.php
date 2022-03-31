def9d723e7676316078c8be4be1558b7bdea1323
Iúri Raimundo <iuraimundo@deloitte.pt>
2022-03-31 16:09:48 +0100

    Mudança

--- utils/preflightHandler.php
+++ utils/preflightHandler.php
@@ -2,6 +2,10 @@
 
     function preflightHandler() {
         if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+            header('Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE');
+            header('Access-Control-Allow-Headers: X-Requested-With');
+            header('Access-Control-Allow-Origin: *');
+            header('Access-Control-Max-Age: 3600');
             http_response_code(200);
             echo json_encode(array("message" => "Success"));
             exit();
--- utils/requestConfig.php
+++ utils/requestConfig.php
@@ -3,9 +3,6 @@
     include 'preflightHandler.php';
 
     function requestConfig() {
-        header('Access-Control-Allow-Origin: *');
-        header('Access-Control-Allow-Methods: GET, POST');
-        header("Access-Control-Allow-Headers: X-Requested-With");
         preflightHandler(); // CORS
         header("Content-Type: application/json; charset=utf-8");
     }
