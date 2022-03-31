cc12ae10264af39b0aa71a18a75a1223cc34c6c3
Iúri Raimundo <iuraimundo@deloitte.pt>
2022-03-31 20:55:28 +0100

    configuração do preflight

--- utils/requestConfig.php
+++ utils/requestConfig.php
@@ -3,9 +3,9 @@
     include 'preflightHandler.php';
 
     function requestConfig($allowed_methods) {
+        header('Access-Control-Allow-Origin: *');
         preflightHandler(); // CORS
         if (in_array($_SERVER['REQUEST_METHOD'], $allowed_methods));
-        header('Access-Control-Allow-Origin: *');
         header("Content-Type: application/json; charset=utf-8");
     }
 
