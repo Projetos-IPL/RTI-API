040aeec8f0e7d49ee21cab5d370b61a11bda5321
Iúri Raimundo <iuraimundo@deloitte.pt>
2022-03-31 15:58:06 +0100

    Method protection on the auth endpoint.

--- utils/requestConfig.php
+++ utils/requestConfig.php
@@ -7,6 +7,6 @@
         header('Access-Control-Allow-Methods: GET, POST');
         header("Access-Control-Allow-Headers: X-Requested-With");
         header("Content-Type: application/json; charset=utf-8");
-        preflightHandler();
+        preflightHandler(); // CORS
     }
 
