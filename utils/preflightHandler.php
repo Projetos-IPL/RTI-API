8c4a79a0bffc9668d71f1826731da6efd7d3a973
Iúri Raimundo <iuraimundo@deloitte.pt>
2022-03-31 19:55:05 +0100

    configuração do preflight

--- utils/preflightHandler.php
+++ utils/preflightHandler.php
@@ -5,7 +5,6 @@
             http_response_code(200);
             header('Access-Control-Allow-Methods: *');
             header('Access-Control-Allow-Headers: *');
-            header('Access-Control-Allow-Origin: *');
             header('Access-Control-Max-Age: 3600');
             echo json_encode(array("message" => "Success"));
             exit();
--- utils/requestConfig.php
+++ utils/requestConfig.php
@@ -3,7 +3,8 @@
     include 'preflightHandler.php';
 
     function requestConfig($allowed_methods) {
-        preflightHandler($allowed_methods); // CORS
+        header('Access-Control-Allow-Origin: *');
         header("Content-Type: application/json; charset=utf-8");
+        preflightHandler($allowed_methods); // CORS
     }
 
