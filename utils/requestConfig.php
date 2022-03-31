cdba93ff336f20c97c88240c89233426506e55fc
Iúri Raimundo <iuraimundo@deloitte.pt>
2022-03-31 16:32:42 +0100

    Mudança

--- utils/requestConfig.php
+++ utils/requestConfig.php
@@ -4,7 +4,7 @@
 
     function requestConfig() {
         header('Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE');
-        header('Access-Control-Allow-Headers: X-Requested-With');
+        header('Access-Control-Allow-Headers: *');
         header('Access-Control-Allow-Origin: *');
         header('Access-Control-Max-Age: 3600');
         preflightHandler(); // CORS
