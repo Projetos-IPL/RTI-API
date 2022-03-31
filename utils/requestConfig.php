781dea335afabe8ae539ece3923c55911ff6bdaa
Iúri Raimundo <iuraimundo@deloitte.pt>
2022-03-31 16:00:31 +0100

    Mudança

--- utils/requestConfig.php
+++ utils/requestConfig.php
@@ -6,7 +6,7 @@
         header('Access-Control-Allow-Origin: *');
         header('Access-Control-Allow-Methods: GET, POST');
         header("Access-Control-Allow-Headers: X-Requested-With");
-        header("Content-Type: application/json; charset=utf-8");
         preflightHandler(); // CORS
+        header("Content-Type: application/json; charset=utf-8");
     }
 
