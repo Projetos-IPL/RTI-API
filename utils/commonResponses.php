73c15894a64374ed5909094414c7b18f60aceffc
Iúri Raimundo <iuraimundo@deloitte.pt>
2022-04-03 15:41:22 +0100

    As respostas agora não executam a função exit()

--- utils/commonResponses.php
+++ utils/commonResponses.php
@@ -3,43 +3,42 @@
     function wrongFormatResponse() {
         http_response_code(400);
         echo json_encode(array("message" => "Request body not properly defined."));
-        exit();
     }
 
     function methodNotAvailable($method) {
         http_response_code(400);
         echo json_encode(array("message" => $method . " is not available in this endpoint."));
-        exit();
     }
+
 
     function internalErrorResponse($message) {
         http_response_code(503);
         echo json_encode(array("message" => $message));
-        exit();
     }
 
     function successfulDataFetchResponse($fetchedData) {
         http_response_code(200);
         echo $fetchedData;
-        exit();
     }
 
     function objectWrittenSuccessfullyResponse($object) {
         http_response_code(200);
         echo json_encode($object);
-        exit();
     }
 
     function objectDeletedSuccessfullyResponse($object) {
         http_response_code(200);
         echo json_encode($object);
-        exit();
     }
 
     function unprocessableEntityResponse($message) {
         http_response_code(422);
         echo json_encode(array("message" => $message));
-        exit();
+    }
+
+    function notAuthrorizedResponse() {
+        http_response_code(401);
+        echo json_encode(array("message" => "Acesso não autorizado."));
     }
 
 
