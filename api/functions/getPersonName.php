d3983faff85c911eea853b2903ddd7ccac9b9fc4
Iúri Raimundo <iuraimundo@deloitte.pt>
2022-04-05 11:53:48 +0100

    alteração dos includes para utilizar absolute paths

---
+++ api/functions/getPersonName.php
@@ -0,0 +1,35 @@
+<?php
+
+    var_dump($_SERVER);
+    exit();
+    include_once $_SERVER['DOCUMENT_ROOT'].'/utils/requestConfig.php';
+    include_once $_SERVER['DOCUMENT_ROOT'].'/utils/commonResponses.php';
+    include_once $_SERVER['DOCUMENT_ROOT'].'/People/PeopleManager.php';
+    include_once $_SERVER['DOCUMENT_ROOT'].'/People/PeopleUtils.php';
+
+    requestConfig();
+
+    $req_body = json_decode(file_get_contents('php://input'), true) ?: array();
+
+    if (!isset($req_body['rfid']) || count($req_body) != 1) {
+        wrongFormatResponse();
+        exit();
+    }
+
+    // Tentar obter pessoa e enviar nome como resposta
+    try {
+        $peopleArr = PeopleManager::getPeople();
+        $index = PeopleUtils::getPersonIndex($req_body['rfid']);
+
+        http_response_code(200);
+        echo json_encode(
+            array(
+                'name'=> $peopleArr[$index]["primNome"] . " " . $peopleArr[$index]["ultNome"]
+            )
+        );
+    } catch (PersonNotFoundException $e) {
+        unprocessableEntityResponse($e->getMessage());
+    } catch (FileReadException $e) {
+        internalErrorResponse($e->getMessage());
+    }
+
