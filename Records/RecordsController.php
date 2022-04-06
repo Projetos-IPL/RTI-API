6e4726afbd6595b21376c0ec298c07f2d6033cec
Iúri Raimundo <iuraimundo@deloitte.pt>
2022-04-06 17:44:00 +0100

    include fixes

--- Records/RecordsController.php
+++ Records/RecordsController.php
@@ -1,6 +1,38 @@
 <?php
 
+    include_once $_SERVER['DOCUMENT_ROOT'].'/utils/constants.php';
     include_once $_SERVER['DOCUMENT_ROOT'].'/utils/Controller.php';
+    include_once $_SERVER['DOCUMENT_ROOT'].'/utils/commonResponses.php';
+    include_once $_SERVER['DOCUMENT_ROOT'].'/utils/requestConfig.php';
 
     abstract class RecordsController extends Controller {
+
+        public static function handleRequest() {
+            requestConfig();
+            self::$REQ_BODY = json_decode(file_get_contents('php://input'), true) ?: array();
+
+            switch ($_SERVER['REQUEST_METHOD']) {
+                case GET:
+                    self::getHandler();
+                    break;
+                case POST:
+                    self::postHandler();
+                default:
+                    methodNotAvailable($_SERVER['REQUEST_METHOD']);
+            }
+        }
+
+        public static function getHandler() {
+            try {
+                $recordsArr = RecordsManager::getRecords();
+                $recordsJSONEncoded = json_encode(array_values($recordsArr));
+                successfulDataFetchResponse($recordsJSONEncoded);
+            } catch (FileWriteException $e) {
+                internalErrorResponse($e->getMessage());
+            }
+        }
+
+
+
+
     }
--- utils/constants.php
+++ utils/constants.php
@@ -1,3 +1,9 @@
 <?php
 
     defined('ROOTPATH') or define('ROOTPATH', __DIR__ . '/../');
+
+    defined('GET') or define('GET', 'GET');
+    defined('POST') or define('POST', 'POST');
+    defined('PUT') or define('PUT', 'PUT');
+    defined('DELETE') or define('DELETE', 'DELETE');
+
