aceaed08b7bd2a277475e7af6e033e06092c750d
Iúri Raimundo <iuraimundo@deloitte.pt>
2022-05-20 17:41:16 +0100

    TRUST

--- api/sensorTypes.php
+++ api/sensorTypes.php
@@ -7,9 +7,11 @@ include_once $_SERVER['DOCUMENT_ROOT'] . '/SensorLogs/SensorLogUtils.php';
 requestConfig();
 
 try {
+
     $pdo = DB::connect();
     $sensorTypes = SensorLogUtils::getSensorTypes($pdo);
     successfulDataFetchResponse(json_encode($sensorTypes));
+
 } catch (DBConnectionException $e) {
     internalErrorResponse($e->getMessage());
 }
