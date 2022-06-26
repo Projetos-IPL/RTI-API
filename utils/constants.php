0992758397c4f85a0729796fcccb9c8bb78efe74
Afonso Santos <afonso.faisca1@gmail.com>
2022-06-26 15:31:05 +0100

    Update constants.php

--- utils/constants.php
+++ utils/constants.php
@@ -20,7 +20,7 @@ defined('ENTITY_NOT_FOUND_INDEX') or define('ENTITY_NOT_FOUND_INDEX', -1);
 defined('DB_HOST') or define('DB_HOST', 'localhost');
 defined('DB_DATABASE_NAME') or define('DB_DATABASE_NAME', 'rti');
 defined('DB_USER') or define('DB_USER', 'root');
-defined('DB_PASSWORD') or define('DB_PASSWORD', '');
+defined('DB_PASSWORD') or define('DB_PASSWORD', 'password');
 defined('DB_CHARSET') or define('DB_CHARSET', 'utf8mb4');
 
 // Event Queue
